93b43544c6e5dc8dd6411b459f3e85f3a4e5f097
Dirk Gustke <dirk.gustke@vmpublishing.com>
2016-08-08 15:37:54 +0200

    master - version bump

--- lib/sensu-plugins-mysql/version.rb
+++ lib/sensu-plugins-mysql/version.rb
@@ -1,9 +1,10 @@
 module SensuPluginsMySql
   module Version
     MAJOR = 0
-    MINOR = 0
-    PATCH = 4
+    MINOR = 1
+    PATCH = 0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
 end
+
